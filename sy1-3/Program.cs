7dd018ce36a138071abd5f15cfbc3aba6034c04a
summerbb <942267539@qq.com>
2017-09-19 23:11:48 +0800

    first commit

---
+++ sy1-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sy1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            while (num < 50)
+            {
+                if (num % 2 == 1 && num % 3 == 1 && num % 4 == 1)
+                {
+                    Console.WriteLine("这篮鸡蛋至少有{0}个", num);
+                    break;
+                }
+                num++;
+            }
+        }
+     }
+    
+}
